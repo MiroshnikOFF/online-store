11d20f04f5699bf03e56963737aa830b9b6f4cc9
miroshnikoff <taxi83080@gmail.com>
2023-09-06 20:23:16 +0300

    add models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    creation_date = models.DateTimeField(verbose_name="Дата создания")
+    last_modified_date = models.DateTimeField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
