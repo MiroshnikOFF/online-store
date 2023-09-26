980696e8b3a4e220a5d9e61aedb8af1479f880b5
miroshnikoff <taxi83080@gmail.com>
2023-09-27 01:40:18 +0300

    refactor urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import HomeTemplateView, contacts, ProductListView
+from catalog.views import HomeTemplateView, contacts, ProductListView, ProductCreateView, ProductUpdateView, \
+    ProductDetailView, ProductDeleteView, get_versions_by_product, show_version
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', HomeTemplateView.as_view(), name='home'),
+
     path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>', ProductListView.as_view(), name='product'),
+
+    path('products/', ProductListView.as_view(), name='products'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+
+    path('products/<int:pk>/versions/', get_versions_by_product, name='product_versions'),
+    path('products/<int:product_pk>/version/<int:version_pk>/', show_version, name='product_version_detail'),
 ]
