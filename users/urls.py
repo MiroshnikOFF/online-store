315e9b89668722e7bb012a3596873b04465b57ed
miroshnikoff <taxi83080@gmail.com>
2023-09-30 23:19:16 +0300

    refactor users urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, LoginView, LogoutView, UserUpdateView, email_verification
+from users.views import RegisterView, LoginView, LogoutView, UserUpdateView, email_verification, password_recovery
 
 app_name = UsersConfig.name
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', UserUpdateView.as_view(), name='profile'),
     path('verification/', email_verification, name='verification'),
+    path('recovery/', password_recovery, name='recovery'),
 ]
